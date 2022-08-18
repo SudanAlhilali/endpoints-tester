b2abb381df1c6ce975ee726e572f4e929440b83a
Sudan Alhilali <sudanalhilali@gmail.com>
2022-08-18 18:39:06 +0300

    remove

--- src/common utils/common-utils.jsx
+++ src/common utils/common-utils.jsx
@@ -2,9 +2,9 @@ const checkValidJson = (text) => {
   if (
     /^[\],:{}\s]*$/.test(
       text
-        .replace(/\\["\\\/bfnrtu]/g, "@")
+        .replace(/\\["\\/bfnrtu]/g, "@")
         .replace(
-          /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g,
+          /"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+-]?\d+)?/g,
           "]"
         )
         .replace(/(?:^|:|,)(?:\s*\[)+/g, "")
